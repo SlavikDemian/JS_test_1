6012c91ac51f5c379598203ef0ae9202bc3c3fdc
SlavikDemian <slavikdemian@gmail.com>
2023-05-12 20:50:37 +0300

    JS vse sam 1

---
+++ js/scripts.js
@@ -0,0 +1,191 @@
+// console.log('Hello')
+//
+//
+// console.log('world')
+
+// let myNumber = 10
+
+// const myNumber = 10
+//
+// console.log(myNumber)
+//
+// myNumber = 5
+//
+// console.log(myNumber)
+// console.log(myNumber)
+// console.log(myNumber)
+// console.log(myNumber)
+
+// const myNumber = 10
+//     myName = "Slavik"
+//     age = 37
+// const message = 'Hello'
+// console.log(message)
+
+// let myNumber = true
+// console.log(typeof myNumber)
+
+
+// const name = 'world'
+// console.log(`Hello ${2*2}
+// world
+// Slavik
+// `)
+
+// const ageSlavik = 2023 - 1985,
+//     ageLarisa = 2023 - 1986
+// console.log(ageSlavik, ageLarisa)
+
+// console.log(2 ** 5)
+
+// console.log('Hello' + ' ' + 'world' + ' my' + ' name' + ' Slavik')
+
+// let a = 2
+// a *= 5
+// console.log(a)
+
+// let a =2
+// a++
+// console.log(a)
+
+
+// console.log(true < false)
+
+// let x = 1
+// x = "1"
+// x = Number(x)
+// console.log(x == 1)
+// Number()
+// String()
+// Boolean()
+
+
+
+// const massPiter = 72,
+//     heighPiter  = 1.88,
+//     massDenis = 82,
+//     heighDenis = 1.73
+//
+// const bmiPiter = massPiter / heighPiter ** 2,
+//     bmiDenis = massDenis / (heighDenis * heighDenis),
+//     bmiHigher = bmiPiter < bmiDenis
+//
+// console.log(bmiHigher)
+
+
+
+// const firstName = prompt('Напишите первое имя'),
+//     secondName = prompt('Напишите второе имя')
+//
+// const randomNumber = (Math.floor(Math.random() * 100))
+//
+// if(randomNumber < 33 && firstName != null && secondName != null && firstName !='' && secondName !='') {
+//     alert(`Результат совместимости имен ${firstName} и ${secondName} равен ${randomNumber} вам нужно поискать человека с другим именем`)
+// } else if (randomNumber >= 33 && randomNumber < 66 && firstName != null && secondName != null && firstName !='' && secondName !='') {
+//     alert(`Результат совместимости имен ${firstName} и ${secondName} равен ${randomNumber} совместимость хорошая, но может быть и лучьше`)
+// } else if (randomNumber >= 66 && firstName != null && secondName != null && firstName !='' && secondName !='') {
+//     alert(`Результат совместимости имен ${firstName} и ${secondName} равен ${randomNumber} вы идеально подходите друг к другу`)
+// } else {
+//     alert('Упс.... вы не ввели имя, попробу еще раз.')
+// }
+
+
+
+
+// const age = 37
+//
+// age >= 18 ? console.log('Мне есть 18, могу пить винишко') : console.log('Я еще маленький, пью сок')
+//
+// const drink = age >= 18 ? 'wine' : 'juice'
+// console.log(drink)
+//
+// let myAge = 25
+//
+// console.log(myAge)
+//
+//     myAge = 30
+// console.log(myAge)
+//
+//     myAge = 35
+// console.log(myAge)
+//
+//
+//     myAge = 37
+// console.log(myAge)
+
+
+// console.log(`one  ${5 * 5}`)
+
+
+// alert('Коты' < 'коты')
+
+// let x = 1
+//     x = 'ten10'
+//     x = Number(x)
+// console.log(typeof x)
+
+
+
+// const weightPetr = 72
+//     heightPetr = 1.88 ** 2
+//     weightDenis = 82
+//     heightDenis = 1.73 ** 2
+//
+//     bmiPetr = weightPetr / heightPetr
+//     bmiDenis = weightDenis / heightDenis
+//
+// console.log(bmiPetr == bmiDenis)
+// console.log(bmiPetr)
+// console.log(bmiDenis)
+
+
+// const firstName = prompt('Введи первое имя')
+//     secondName = prompt('Введите второе имя')
+//     randomNumber = Math.floor(Math.random() * 100)
+//
+// alert(`Ваш индекс совместимост ${firstName} и ${secondName} равен ${randomNumber}`)
+
+
+
+// const firstName = prompt('Введи первое имя')
+//     secondName = prompt('Введите второе имя')
+//     randomNumber = Math.floor(Math.random() * 100)
+//
+// if (randomNumber < 33 && firstName != null && secondName != null && firstName !='' && secondName !='') {
+//     alert(`Ваш индекс совместимост ${firstName} и ${secondName} равен ${randomNumber} нужен другой`)
+// } else if (randomNumber >= 33 && randomNumber < 66 && firstName != null && secondName != null && firstName !='' && secondName !='') {
+//     alert(`Ваш индекс совместимост ${firstName} и ${secondName} равен ${randomNumber} неплохо, но можно и лучше`)
+// } else if (randomNumber >= 66 && firstName != null && secondName != null && firstName !='' && secondName !='') {
+//     alert(`Ваш индекс совместимост ${firstName} и ${secondName} равен ${randomNumber} идиально`)
+// } else {
+//     alert('Еще раз, ты ничего не написал')
+// }
+//
+// console.log(randomNumber)
+
+
+let age = prompt('Вадос, напиши свой возраст',)
+
+let message = (age < 7) ? 'пиздишь что мелкий ха-ха-ха' :
+    (age < 18) ? 'давай правду, в этом возрасте не наливают!' :
+        (age < 100) ? 'Урааа, тебе больше 18, можно пить винишкооо )))' :
+            'ого O_o';
+alert(message);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
