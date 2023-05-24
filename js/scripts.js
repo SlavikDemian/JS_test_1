8e7c5ae43c1d96458d54e2626a994035c1d85533
SlavikDemian <slavikdemian@gmail.com>
2023-05-24 18:29:30 +0300

    JS leson 1

--- js/scripts.js
+++ js/scripts.js
@@ -164,18 +164,380 @@
 // console.log(randomNumber)
 
 
-let age = prompt('Вадос, напиши свой возраст',)
-
-let message = (age < 7) ? 'пиздишь что мелкий ха-ха-ха' :
-    (age < 18) ? 'давай правду, в этом возрасте не наливают!' :
-        (age < 100) ? 'Урааа, тебе больше 18, можно пить винишкооо )))' :
-            'ого O_o';
-alert(message);
-
-
+// let age = prompt('Вадос, напиши свой возраст',)
+//
+// let message = (age < 7) ? 'пиздишь что мелкий ха-ха-ха' :
+//     (age < 18) ? 'давай правду, в этом возрасте не наливают!' :
+//         (age < 100) ? 'Урааа, тебе больше 18, можно пить винишкооо )))' :
+//             'ого O_o';
+// alert(message);
 
 
+// 'use strict'
+//
+// let hasDriveLicence = false
+// const passTest = true
+//
+// if (passTest) {
+//     hasDrivesLicence = true
+// }
+// if (hasDriveLicence) {
+//     console.log ('я водитель')
+// }
 
+
+// 'use strict'
+
+// function multiConsole() {
+//     console.log('hello!!')
+//     console.log('hello!!')
+//     console.log('hello!!')
+//     console.log('hello!!')
+//     console.log('hello!!')
+// }
+// const a = multiConsole()
+// console.log(a)
+
+
+
+// function calc(yapohka, haurmyau, tonni, hashik, suhi, hruhi) {
+//
+//     return yapohka + haurmyau * tonni - hashik
+// }
+// const result = calc(10, 4, 7, 50, 8,5, )
+// console.log(result)
+
+
+
+
+// // function declaration
+// function calc(apple, orange){
+//     return apple + orange
+// }
+// const result = calc(10, 5)
+// console.log(result)
+//
+//
+// // function expression
+// let result2 = function (a, b, c){
+//     return a + b - c
+// }
+// console.log(result2(10, 50, 5))
+
+
+
+// let a = 12
+// function myFunction() {
+//     a = 10
+// }
+// myFunction()
+// console.log(a)
+
+
+
+// const a = 3
+// function myFunction (){
+//     function myFunction2(){
+//         console.log(a)
+//     }
+//     myFunction2()
+// }
+// myFunction()
+
+
+// let a
+// let b
+// function myFn(){
+//     let b
+//     a = true
+//     b = 5
+//     console.log(b)
+// }
+// myFn()
+// console.log(a)
+// console.log(b)
+
+
+// function myFn(a = "Число ", b = 7 + 2 ){
+// console.log(a + b)
+// }
+// myFn("номер ")
+
+
+// 'use strict'
+
+// confirm('Сколько?')
+
+// function ask(question, yes, no){
+//     if(confirm(question)){
+//         yes()
+//     }else no()
+// }
+// function showOk(){
+//     alert('спасибо!!!')
+// }
+// function showCancel(){
+//     alert('отмена!!!!')
+// }
+// ask('Согласны?', showOk, showCancel)
+
+
+
+/*  const
+    vesPetr = 72,
+    rostPetr = 1.88,
+    vesDenis = 82,
+    rostDenis = 1.72,
+    bmiPetr = vesPetr / rostPetr ** 2,
+    bmiDenis = vesDenis / rostDenis ** 2
+console.log(bmiDenis < bmiPetr)
+*/
+
+
+
+/*
+const firstName = prompt('Первое имя'),
+    secondName = prompt('Второе имя'),
+    randomNumber = Math.floor(Math.random() * 100)
+
+if (randomNumber <= 33 && firstName != null && secondName != null  && firstName != '' && secondName != '') {
+    alert(`Итог ${firstName} + ${secondName} = ${randomNumber} поищи еще`)
+} else if (randomNumber >= 34 && randomNumber <= 66 && firstName != null && secondName != null && firstName != '' && secondName != '') {
+    alert(`Итог ${firstName} + ${secondName} = ${randomNumber} неплохо, но может біть лучьше`)
+} else if (randomNumber > 67 && firstName != null && secondName != null && firstName != '' && secondName != '') {
+    alert(`Итог ${firstName} + ${secondName} = ${randomNumber} супер!!!`)
+} else {
+    alert(` что-то пошло не так, еще раз`)
+}
+*/
+
+'use strict'
+
+
+/*
+ function calcMy(a, b, c){
+     return a + b - c
+ }
+
+// const result = calcMy()
+console.log(calcMy(10, 5 , 7))
+*/
+
+
+/*
+function myFu(a = 'ваше число ', b = 7 + 2 - 1) {
+    console.log(a, b)
+
+}
+myFu('твой номер')
+*/
+
+
+/*
+function ask(a, b, c) {
+    if(confirm(a)){
+        b()
+    }else c()
+}
+
+// function showOk(){
+//     alert('спасибо')
+// }
+// function showCancel (){
+//     alert('отмена')
+// }
+
+ask('согласні?',  () => alert('спасибо'), () => alert('отмена') )
+*/
+
+
+// const crazyKatsRes1 = 44,
+//     crazyKatsRes2 = 23,
+//     crazyKatsRes3 = 71,
+//
+//     funnyDucksRes1 = 65,
+//     funnyDucksRes2 = 54,
+//     funnyDucksRes3 = 49,
+
+// const srediiResKats = (res1, res2, res3) => {
+//     return (res1 + res2 + res3) / 3
+// }
+// srediiResKats (44, 23, 71)
+// console.log(srediiResKats(44,23,71))
+//
+//
+// const srediiResDuks = (res1, res2, res3) => {
+//     return (res1 + res2 + res3) / 3
+// }
+// srediiResKats (65, 54, 49)
+// console.log(srediiResDuks(65,54,49))
+//
+// function (srediiResKats, srediiResDuks) {
+//     if (srediiResKats)
+// }
+
+
+
+
+/*
+const calcAverage = (res1, res2, res3) => (res1 + res2 + res3) / 2
+
+const scoreKats = calcAverage(44, 23, 71)
+const scoreDucks = calcAverage(65, 54, 49)
+
+function checkWinner(avgKats, avgDucks) {
+    if (avgKats >= 2 * avgDucks) {
+        console.log(`crazyKats победили с результатом ${avgKats} против funnyDucks с результатом ${avgDucks}`)
+    } else if (avgDucks >= 2 * avgKats) {
+        console.log(`funnyDucks победили с результатом ${avgDucks} против razyKats с результатом ${avgKats}`)
+    } else {
+        console.log ('У нас ничья!!!')
+    }
+}
+checkWinner(scoreKats, scoreDucks)
+*/
+
+
+
+
+
+/*
+const slavik = {
+    firstName: 'Demianenko',
+    age: 37,
+    sex: 'male',
+    height: 190,
+    weight: 100
+}
+console.log(slavik)
+
+slavik.weight = 85
+slavik.age = 35
+console.log(slavik)
+
+delete slavik.firstName
+console.log(slavik)
+
+slavik['sex'] = 'male hamale)))'
+console.log(slavik)
+
+delete slavik['height']
+console.log(slavik)
+
+slavik['firstName'] = 'Demianenko'
+console.log(slavik)
+
+const propertyName = 'newProperty'
+console.log(propertyName)
+
+
+// slavik.propertyName = 'propertyValue'
+// console.log(slavik)
+
+slavik[propertyName + ' 3'] = 'propertyValue'
+console.log(slavik)
+
+
+console.log(slavik['newProperty 3'])
+
+const answer = prompt('Напишите слово firstName или age')
+alert(slavik[answer])
+*/
+
+
+
+
+/*
+const slavik = {
+    firstName: 'Demianenko',
+    age: 37,
+    sex: 'male',
+    height: 190,
+    weight: 100,
+    placeOfBirth: {
+        country: 'Ukraine',
+        city: 'Odesa'
+    }
+}
+console.log(slavik)
+
+slavik.placeOfBirth.country = 'Canada'
+console.log(slavik)
+
+slavik.placeOfBirth['dateOfBirth'] = 1985
+console.log(slavik)
+
+delete slavik["placeOfBirth"].city
+*/
+
+
+
+/*
+const firstName = 'John'
+const lastName = 'Travolta'
+const slavik = {
+    firstName,
+    lastName,
+    age: 37,
+    sex: 'male',
+    placeOfBirth: {
+        country: 'Ukraine',
+        city: 'Odesa'
+    }
+}
+console.log(slavik)
+*/
+
+
+
+/*
+const firstName = 'John'
+const lastName = 'Travolta'
+
+function calcAge(){
+    return 2023 - slavik.birthYear
+}
+
+const slavik =  {
+    firstName,
+    lastName,
+    birthYear: 1985,
+    // calcAge: function (){
+    //     console.log(this)
+    //     return 2023 - this.birthYear
+    // }
+    calcAge,
+}
+console.log(slavik.calcAge())
+*/
+
+
+
+
+/*
+const firstName = 'John'
+const lastName = 'Travolta'
+
+function calcAge(){
+    return 2023 - this.birthYear
+}
+
+const slavik =  {
+    firstName,
+    lastName,
+    birthYear: 1985,
+    calcAge,
+}
+const larisa =  {
+    firstName,
+    lastName,
+    birthYear: 1986,
+    calcAge,
+}
+
+console.log(slavik.calcAge())
+console.log(larisa.calcAge())
+*/
 
 
 
